e00f8fc644d9dc4dd746a62d9d4927bdc797a473
MarthaFathy-1 <fathimartha@gmail.com>
2025-11-02 14:02:27 +0200

    Add project files.

---
+++ TaskManager/Models/TaskItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Display(Name = "Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Is completed?")]
+        public bool IsCompleted { get; set; }
+    }
+}
